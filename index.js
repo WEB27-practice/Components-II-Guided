a0d111833fcad78b885671d4d6899a1f1946bdea
seanaleid <seanaleid@gmail.com>
2020-01-16 13:41:46 -0500

    Add axios get requests to loop over and create cards

--- index.js
+++ index.js
@@ -24,10 +24,29 @@ function dogCard(imgUrl) {
 // Step Three: talk about HTTP, requesting data from a server, and axios
 // with get(), we send a GET request
 
-axios.get('https://dog.ceo/api/breed/pembroke/images/random/12')
+axios.get('https://dog.ceo/api/breed/weimaraner/images/random/12')
     .then(res => {
         console.log(res);
+        res.data.message.forEach(data => entryPoint.append(dogCard(data)));
     })
     .catch( error => {
         console.log("the data was not returned", error)
-      })
+    })
+
+axios.get('https://dog.ceo/api/breed/pembroke/images/random/12')
+    .then(res => {
+        console.log(res);
+        res.data.message.forEach(data => entryPoint.append(dogCard(data)));
+    })
+    .catch( error => {
+        console.log("the data was not returned", error)
+    })
+
+    axios.get('https://dog.ceo/api/breed/hound/blood/images/random/12')
+    .then(res => {
+        console.log(res);
+        res.data.message.forEach(data => entryPoint.append(dogCard(data)));
+    })
+    .catch( error => {
+        console.log("the data was not returned", error)
+    })
