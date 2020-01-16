69257728c0071f463c5f737640d65ef1472d2054
seanaleid <seanaleid@gmail.com>
2020-01-16 13:14:30 -0500

    Add a function to build out card structure

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 // Step Four: Build out a basic DogCard component, we should only concern ourselves with displaying the image at this point.
+const entryPoint = document.querySelector('.entry');
+console.log(entryPoint);
+
+function dogCard(imgUrl) {
+    const newCard = document.createElement('div'),
+        newImage = document.createElement('img'),
+        breed = document.createElement('h3');
+
+    breed.textContent = "Breed: Corgi";
+    newImage.src = imgUrl;
+
+    newImage.classList.add('dog-image');
+    newCard.classList.add('dog-card');
+
+    newCard.append(newImage, breed);
+    // newCard.append(breed);
+
+    return newCard;
+}
+
 
 
 // Step Three: talk about HTTP, requesting data from a server, and axios
+// with get(), we send a GET request
 
+axios.get('https://dog.ceo/api/breed/pembroke/images/random/12')
+    .then(res => {
+        console.log(res);
+    })
+    .catch( error => {
+        console.log("the data was not returned", error)
+      })
