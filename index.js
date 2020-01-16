a1ccc41af00775ab8668ad5505662f70a1134511
seanaleid <seanaleid@gmail.com>
2020-01-16 12:09:42 -0500

    Initialize files to begin repo (html, css, js)

---
+++ index.js
@@ -0,0 +1,5 @@
+// Step Four: Build out a basic DogCard component, we should only concern ourselves with displaying the image at this point.
+
+
+// Step Three: talk about HTTP, requesting data from a server, and axios
+
